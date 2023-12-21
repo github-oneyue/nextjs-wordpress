89cc668b2863dcca66d0c68ac174dd3292ec78e2
Oneyue <74414063+github-oneyue@users.noreply.github.com>
2023-12-21 21:46:28 +0800

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 const config = {
   siteName: 'Next.js WordPress',
   siteDescription: "It's headless WordPress!",
-  siteUrl: 'https://nextjswp.com'
+  siteUrl: 'http://tiktv.eu.org'
 }
 
 export default config
